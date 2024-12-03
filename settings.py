da0210a75bf4ea3b0e01ab536cbdcd97f29bcddb
Owen <owenrees82@gmail.com>
2024-12-04 00:48:36 +0100

    no file deletion

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ load_dotenv()
 
 API_KEY = os.getenv('PLANTNET_API_KEY')
 FILE_SERVER_SECRET_KEY = os.getenv('FILE_SERVER_SECRET_KEY')
+FILE_SERVER_REMOTE_URL = os.getenv('FILE_SERVER_REMOTE_URL')
 
 SCORE_LOWER_THRESHOLD = 30  # minimum score for a result to be considered valid
 ALTERNATIVE_SCORE_LOWER_THRESHOLD = 10  # minimum score for an alternative result to be considered valid
@@ -16,7 +17,7 @@ headers = {
                       'Chrome/50.0.2661.102 Safari/537.36'}
 
 FILE_SERVER_PORT = 4321
-FILE_SERVER_BASE_URL = f"http://localhost:{FILE_SERVER_PORT}"
-# FILE_SERVER_BASE_URL = f"http://81.28.6.144:{FILE_SERVER_PORT}"
+# FILE_SERVER_BASE_URL = f"http://localhost:{FILE_SERVER_PORT}"
+FILE_SERVER_BASE_URL = FILE_SERVER_REMOTE_URL
 FILE_SERVER_URL = f"{FILE_SERVER_BASE_URL}/plant_id"
-FILE_SERVER_IMAGES_URL = f"{FILE_SERVER_BASE_URL}/public/plant_id"
+FILE_SERVER_IMAGES_URL = f"{FILE_SERVER_BASE_URL}/plant_id"
